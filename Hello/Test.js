94f2cb106fad47502b1ef8d2932e565f40e1d634
Zukilprime <zukil@juno.com>
2020-04-13 20:40:16 -0500

    Updates

---
+++ Hello/Test.js
@@ -0,0 +1,20 @@
+var msg = "Hello World";
+console.log(msg);
+
+let x = 7;
+let y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+var bundle = [1, 2, 3, 4, 5];
+console.log(bundle[2]);
+
+for (var i = 0; i < 6; i++) {
+  console.log(i);
+}
+console.log(i);
+
+var msgUpper = msg.toUpperCase();
+console.log(msgUpper);
